10e0ea0cc7aeadfb2f2b7f71bae00bc3107f3b84
May <112887245+maynadimoe@users.noreply.github.com>
2023-04-24 22:21:42 +0630

    Made some final changes

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,15 +7,24 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
+
       <div className="row mt-3">
         <div className="col-6">
-          <div className="d-flex">
+          <span>
+            <FormattedDate date={props.data.date} />
+          </span>
+          <span className="text-capitalize">{props.data.description}</span>
+          <span>
+            <br />
+            Humidity: {props.data.humidity}%
+          </span>
+          <span>
+            {" "}
+            <br /> Wind: {props.data.wind} km/h
+          </span>
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-8">
+          <div className="d-flex justify-content-end">
             <div>
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
@@ -24,12 +33,6 @@ export default function WeatherInfo(props) {
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
-          </ul>
         </div>
       </div>
     </div>
